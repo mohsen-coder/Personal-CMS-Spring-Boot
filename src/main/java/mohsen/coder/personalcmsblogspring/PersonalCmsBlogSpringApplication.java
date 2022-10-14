eca29ff1e1372cd8a460cdb0486fbc389ad3d348
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-10-14 19:09:59 +0330

    initialize project

---
+++ src/main/java/mohsen/coder/personalcmsblogspring/PersonalCmsBlogSpringApplication.java
@@ -0,0 +1,13 @@
+package mohsen.coder.personalcmsblogspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCmsBlogSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalCmsBlogSpringApplication.class, args);
+    }
+
+}
