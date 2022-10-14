b4c384fff4074dbaa14039e958376e0c38017508
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-10-14 19:27:08 +0330

    new(ApiError.java): create for send custom error message to client

---
+++ src/main/java/mohsen/coder/personalcmsblogspring/errors/ApiError.java
@@ -0,0 +1,29 @@
+package mohsen.coder.personalcmsblogspring.errors;
+
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ApiError {
+    private int status;
+    private final List<String> messages = new ArrayList<>();
+
+    public ApiError() {
+    }
+
+    public ApiError(HttpStatus status) {
+        this.status = status.value();
+    }
+
+    public void addMessage(String message){
+        messages.add(message);
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
