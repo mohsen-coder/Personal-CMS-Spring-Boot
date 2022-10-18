be0df9b3d5fe555c380ac3573af727704f903064
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-10-18 21:48:50 +0330

    update(InvalidFieldException.java): add a field for global errors

--- src/main/java/mohsen/coder/personalcmsblogspring/errors/InvalidFieldException.java
+++ src/main/java/mohsen/coder/personalcmsblogspring/errors/InvalidFieldException.java
@@ -1,16 +1,23 @@
 package mohsen.coder.personalcmsblogspring.errors;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class InvalidFieldException extends Exception {
+    private final ArrayList<String> globalFieldsErrors;
     private final List<Map<String, String>> fields;
 
-    public InvalidFieldException(List<Map<String, String>> fields) {
+    public InvalidFieldException(List<Map<String, String>> fields, ArrayList<String> globalFieldsErrors) {
         this.fields = fields;
+        this.globalFieldsErrors = globalFieldsErrors;
     }
 
     public List<Map<String, String>> getFields() {
         return fields;
+    }
+
+    public ArrayList<String> getGlobalFieldsErrors() {
+        return globalFieldsErrors;
     }
 }
