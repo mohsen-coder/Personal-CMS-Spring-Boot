800add9bf1cdb737dd94be3d238e3cae82091c4e
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-10-24 22:55:30 +0330

    new(GetAccountUseCase.java)

--- src/main/java/mohsen/coder/personalcmsblogspring/application/port/in/GetAccountUseCase.java
+++ src/main/java/mohsen/coder/personalcmsblogspring/application/port/in/GetAccountUseCase.java
@@ -1,19 +1,19 @@
 package mohsen.coder.personalcmsblogspring.application.port.in;
 
-import mohsen.coder.personalcmsblogspring.domain.Account;
+import mohsen.coder.personalcmsblogspring.adapter.web.model.AccountModel;
 import mohsen.coder.personalcmsblogspring.errors.NotFoundException;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Collection;
 
 public interface GetAccountUseCase {
-    ResponseEntity<Account> getAccountById(String accountId) throws NotFoundException;
+    ResponseEntity<AccountModel> getAccountById(String accountId) throws NotFoundException;
 
-    ResponseEntity<Account> getAccountByUsername(String username) throws NotFoundException;
+    ResponseEntity<AccountModel> getAccountByUsername(String username) throws NotFoundException;
 
-    ResponseEntity<Account> getAccountByEmail(String email) throws NotFoundException;
+    ResponseEntity<AccountModel> getAccountByEmail(String email) throws NotFoundException;
 
-    ResponseEntity<Collection<Account>> getAccountsByRole(String role, int skip, int limit);
+    ResponseEntity<Collection<AccountModel>> getAccountsByRole(String role, int skip, int limit);
 
-    ResponseEntity<Collection<Account>> getAccountsByPagination(int skip, int limit);
+    ResponseEntity<Collection<AccountModel>> getAccountsByPagination(int skip, int limit);
 }
