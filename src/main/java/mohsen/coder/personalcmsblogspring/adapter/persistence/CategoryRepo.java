124083138cd7d2f74eac24aaebde6a447da1ec39
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-10-24 22:18:29 +0330

    new(CategoryRepo.java): define get and delete category functionalities

---
+++ src/main/java/mohsen/coder/personalcmsblogspring/adapter/persistence/CategoryRepo.java
@@ -0,0 +1,18 @@
+package mohsen.coder.personalcmsblogspring.adapter.persistence;
+
+import mohsen.coder.personalcmsblogspring.adapter.persistence.model.CategoryEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepo extends PagingAndSortingRepository<CategoryEntity, Long> {
+    Optional<CategoryEntity> findCategoryEntityByPublicId(String publicId);
+
+    Optional<CategoryEntity> findCategoryEntityByTitle(String title);
+
+    List<CategoryEntity> findAllByStatus(String status, Pageable pageable);
+
+    boolean deleteCategoryEntityByPublicId(String publicId);
+}
