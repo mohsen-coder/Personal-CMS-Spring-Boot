9f5dde9f58a0bff1afff0eacfaa38cb5bae6cf3d
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-10-25 11:49:49 +0330

    update(CategoryController.java): add get category request end points

--- src/main/java/mohsen/coder/personalcmsblogspring/adapter/web/CategoryController.java
+++ src/main/java/mohsen/coder/personalcmsblogspring/adapter/web/CategoryController.java
@@ -3,8 +3,11 @@ package mohsen.coder.personalcmsblogspring.adapter.web;
 import lombok.extern.slf4j.Slf4j;
 import mohsen.coder.personalcmsblogspring.adapter.web.model.CategoryModel;
 import mohsen.coder.personalcmsblogspring.application.port.in.CreateCategoryUseCase;
+import mohsen.coder.personalcmsblogspring.application.port.in.GetCategoryUseCase;
 import mohsen.coder.personalcmsblogspring.errors.ConflictException;
 import mohsen.coder.personalcmsblogspring.errors.InvalidFieldException;
+import mohsen.coder.personalcmsblogspring.errors.NotFoundException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -21,12 +25,13 @@ import java.util.Map;
 public class CategoryController {
 
     private final CreateCategoryUseCase createCategoryUseCase;
+    private final GetCategoryUseCase getCategoryUseCase;
 
-    public CategoryController(CreateCategoryUseCase createCategoryUseCase) {
+    public CategoryController(CreateCategoryUseCase createCategoryUseCase, GetCategoryUseCase getCategoryUseCase) {
         this.createCategoryUseCase = createCategoryUseCase;
+        this.getCategoryUseCase = getCategoryUseCase;
     }
 
-    
     private void requestValidation(Errors errors) throws InvalidFieldException {
         if (errors.hasErrors()) {
             List<Map<String, String>> errorFields = new ArrayList<>();
@@ -43,6 +48,23 @@ public class CategoryController {
 
             throw new InvalidFieldException(errorFields, globalErrors);
         }
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<CategoryModel> getCategoryById(@PathVariable("categoryId") String categoryId)
+            throws NotFoundException {
+        return getCategoryUseCase.getCategoryById(categoryId);
+    }
+
+    @GetMapping(params = { "title" })
+    public ResponseEntity<CategoryModel> getCategoryByTitle(@RequestParam String title) throws NotFoundException {
+        return getCategoryUseCase.getCategoryByTitle(title);
+    }
+
+    @GetMapping(params = { "status", "skip", "limit" })
+    public ResponseEntity<Collection<CategoryModel>> getCategoryByStatusAndPagination(@RequestParam String status,
+            @RequestParam int skip, @RequestParam int limit) {
+        return getCategoryUseCase.getAllCategories(status, skip, limit);
     }
 
     @PostMapping(consumes = "application/json")
