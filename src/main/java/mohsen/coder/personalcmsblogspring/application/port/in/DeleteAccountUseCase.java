884dbb8c89de1630344ec0564aeb20d4e3da43d6
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-10-25 11:03:53 +0330

    update: remove unused packages

--- src/main/java/mohsen/coder/personalcmsblogspring/application/port/in/DeleteAccountUseCase.java
+++ src/main/java/mohsen/coder/personalcmsblogspring/application/port/in/DeleteAccountUseCase.java
@@ -1,6 +1,5 @@
 package mohsen.coder.personalcmsblogspring.application.port.in;
 
-import mohsen.coder.personalcmsblogspring.domain.Account;
 import org.springframework.http.ResponseEntity;
 
 public interface DeleteAccountUseCase {
--- src/main/java/mohsen/coder/personalcmsblogspring/application/port/out/DeleteAccountPort.java
+++ src/main/java/mohsen/coder/personalcmsblogspring/application/port/out/DeleteAccountPort.java
@@ -1,6 +1,5 @@
 package mohsen.coder.personalcmsblogspring.application.port.out;
 
-import mohsen.coder.personalcmsblogspring.domain.Account;
 
 public interface DeleteAccountPort {
     boolean deleteAccount(String accountPublicId);
